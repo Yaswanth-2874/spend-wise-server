484327a0f4391bd52ee43bd90d2d7afd03cc8d79
Yaswanth <yryr2004@gmail.com>
2024-04-30 13:00:09 +0530

    Added authentication without third parties. Cryptographic hashing is used for safety

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,73 @@
 const express = require("express");
 const router = express.Router();
-const { client, connect } = require("../connection");
+const { client, connect, ObjectId } = require("../connection");
 connect();
 const { format } = require("date-fns");
+const bcrypt = require("bcrypt");
 
-router.get("/", async (req, res) => {
+const hashPassword = async (password) => {
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+};
+
+router.route("/").get(async (req, res) => {
   try {
     const data = client.db("ToDoList").collection("SpendWise");
     const query = await data.find({}).toArray();
     res.json(query);
   } catch (e) {
-    console.log("Not able to get data due to ", e);
-    res.json({ status: "Error" });
+    console.log("Error in fetching data due to ", e);
+    res.json({ status: "bad" });
+  }
+});
+router.route("/login").post(async (req, res) => {
+  try {
+    const data = client.db("ToDoList").collection("SpendWise");
+    const { password, email } = req.body;
+    const query = await data.findOne({ email: email });
+    if (!query) {
+      res.json({ status: "bad", reason: "Username not found" });
+      return;
+    }
+    const isPasswordValid = await bcrypt.compare(password, query.password);
+    if (!isPasswordValid) {
+      res.json({ status: "bad", reason: "Username or password wrong" });
+      return;
+    }
+    loggedIn = true;
+    res.json(query);
+  } catch (e) {
+    console.log("Failed to login in due to ", e);
+    res.json({ status: "bad" });
   }
 });
 
+router.post("/register", async (req, res) => {
+  try {
+    const data = client.db("ToDoList").collection("SpendWise");
+    const { email, password } = req.body;
+    const hashedPassword = await hashPassword(password);
+    const newData = {
+      email: email,
+      password: hashedPassword,
+      status: "good",
+      balance: 10000,
+      spendings: {},
+    };
+    await data.insertOne(newData);
+    res.json({ status: "good" });
+  } catch (e) {
+    console.log("Not able to register due to ", e);
+    res.json({ status: "bad" });
+  }
+});
 router
   .route("/:email")
-  .get(async (req, res) => {
-    try {
-      const data = client.db("ToDoList").collection("SpendWise");
-      const query = await data.findOne({ email: req.params.email });
-      if (!query) res.status(500).json({ status: "Data not found" });
-      else res.json(query);
-    } catch (e) {
-      console.log("Not able to get data due to ", e);
-      res.json({ status: "Error" });
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      const data = client.db("ToDoList").collection("SpendWise");
-      const newData = {
-        email: req.params.email,
-        balance: 10000,
-        spendings: {},
-      };
-      await data.insertOne(newData);
-      res.json({ status: "Inserted Data" });
-    } catch (e) {
-      console.log("Not able to insert data due to ", e);
-      res.json({ status: "Error" });
-    }
-  })
   .put(async (req, res) => {
     try {
       const data = client.db("ToDoList").collection("SpendWise");
-      const time = format(new Date(), "HH:mm:ss");
       const { category, description, cost, balance } = req.body;
       let { date } = req.body;
       date = !date ? format(new Date(), "dd-MM-yyyy") : date;
@@ -55,7 +76,12 @@ router
         {
           $set: { balance: balance },
           $addToSet: {
-            [`spendings.${category}`]: { description, cost, time, date },
+            [`spendings.${category}`]: {
+              _id: new ObjectId(),
+              description,
+              cost,
+              date,
+            },
           },
         },
         { upsert: true }
@@ -65,5 +91,27 @@ router
       console.log("Not able to update data due to ", e);
       res.json({ status: "Error" });
     }
+  })
+  .delete(async (req, res) => {
+    try {
+      const data = client.db("ToDoList").collection("SpendWise");
+      await data.deleteOne({ email: req.params.email });
+      res.json({ status: "Deleted Data" });
+    } catch (e) {
+      console.log("Not able to insert data due to ", e);
+      res.json({ status: "Error" });
+    }
   });
+
+router.delete("/items/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = client.db("ToDoList").collection("SpendWise");
+    await data.deleteOne({ _id: new ObjectId(id) });
+    res.json({ status: "good" });
+  } catch (e) {
+    console.log("Not able to delete data with specific id due to ", e);
+    res.json({ status: "bad" });
+  }
+});
 module.exports = router;
