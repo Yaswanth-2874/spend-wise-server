e9e94b1442e295234ac9095ce4397427ee3c3443
Yaswanth <yryr2004@gmail.com>
2025-05-27 07:51:15 +0530

    Update File Structure

--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
-const authRoute = express.Router();
-
-authRoute.post("/login", login);
-authRoute.post("/logout", logout);
-authRoute.post("/signup", signup);
-
-export default authRoute;
